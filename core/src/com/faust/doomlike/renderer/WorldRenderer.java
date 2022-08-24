b06955a0e8734332b18c29ee37834f3cd692a105
Faust <jacopo.buttiglieri@gmail.com>
2022-08-24 17:48:00 +0200

    improvements

--- core/src/com/faust/doomlike/renderer/WorldRenderer.java
+++ core/src/com/faust/doomlike/renderer/WorldRenderer.java
@@ -1,16 +1,13 @@
 package com.faust.doomlike.renderer;
 
-import com.faust.doomlike.test.PlayerInstance;
-import com.faust.doomlike.utils.MapWrapper;
-
 /**
  * World renderer interface
  *
  * @author Jacopo "Faust" Buttiglieri
  */
-public interface WorldRenderer {
+public interface WorldRenderer <T>{
 
-    void drawWorld(MapWrapper map, PlayerInstance playerInstance);
+    void drawWorld(float stateTime, T world);
 
     void dispose();
 }
